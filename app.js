c9bd1b387eea81829a32d42fabd9cd2395d8da83
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-09-06 08:37:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const boxes = document.querySelectorAll(".box"),
+    image = document.querySelector(".image");
+
+boxes.forEach((box) => {
+    box.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+    box.addEventListener("drop", () => {
+        box.appendChild(image);
+    });
+});
+
+
